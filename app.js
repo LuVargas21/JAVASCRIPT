aff2dd593207a641c7860d26bcf40371c1f20255
LuVargas21 <96500749+LuVargas21@users.noreply.github.com>
2022-07-02 11:31:00 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// SIMULADOR INTERACTIVO 
+//UNA APP QUE CALCULE EL COSTO DE DISTINTAS OPCIONES DE ALOJAMIENTO. 
+
+function costoDeViaje () {
+    let ingreseUnDestino = prompt ("Ingrese un destino");
+    let n_noches = n; 
+    let resultado1 = hotel * n_noches;
+    
+}
+
+console.log (ingreseUnDestino)
