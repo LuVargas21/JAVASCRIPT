0d170b5c78d48fd5321c337649b9bdb40ef43b69
LuVargas21 <lucianasalome94@gmail.com>
2022-07-03 15:33:58 -0300

    Update simuladorInteractivo.js

--- simuladorInteractivo.js
+++ simuladorInteractivo.js
@@ -1,11 +1,25 @@
 // SIMULADOR INTERACTIVO 
-//UNA APP QUE CALCULE EL COSTO DE DISTINTAS OPCIONES DE ALOJAMIENTO. 
+//UNA APP QUE CALCULE EL COSTO DE DISTINTAS OPCIONES DE ALOJAMIENTO.
+let destinoIngresado = prompt("Ingrese un destino nacional");
+let cantidadNochesIngresadas = prompt("Ingrese el numero de noches de su estadia");
 
-function costoDeViaje () {
-    let ingreseUnDestino = prompt ("Ingrese un destino");
-    let n_noches = n; 
-    let resultado1 = hotel * n_noches;
-    
+function calcularPrecioEstadia(destino, cantidadNoches) {
+    let precioDeEstadia = cantidadNoches * calcularPrecioPorNoche(destino);
+    return precioDeEstadia;
 }
 
-console.log (ingreseUnDestino)
+// devuelve el precio por noche de acuerdo al destino pasado por parametro
+function calcularPrecioPorNoche(destino) {
+    let precioXNoche = 0;
+    switch (destino) {
+        case "mendoza":
+            precioXNoche = 3500;
+            break;
+        default:
+            precioXNoche = 4000;
+            break;
+    }
+    return precioXNoche;
+}
+
+alert("El precio de la estadia es: " + calcularPrecioEstadia(destinoIngresado, cantidadNochesIngresadas));
