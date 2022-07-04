5fa691ca70e5d545a4c8b01b42d55f7c33f9c650
LuVargas21 <96500749+LuVargas21@users.noreply.github.com>
2022-07-04 13:01:34 -0300

    Update simuladorInteractivo.js
    destinos

--- simuladorInteractivo.js
+++ simuladorInteractivo.js
@@ -1,25 +1,38 @@
-// SIMULADOR INTERACTIVO 
+// SIMULADOR INTERACTIVO
 //UNA APP QUE CALCULE EL COSTO DE DISTINTAS OPCIONES DE ALOJAMIENTO.
 let destinoIngresado = prompt("Ingrese un destino nacional");
-let cantidadNochesIngresadas = prompt("Ingrese el numero de noches de su estadia");
+let cantidadNochesIngresadas = prompt(
+	"Ingrese el numero de noches de su estadia"
+);
 
 function calcularPrecioEstadia(destino, cantidadNoches) {
-    let precioDeEstadia = cantidadNoches * calcularPrecioPorNoche(destino);
-    return precioDeEstadia;
+	let precioDeEstadia = cantidadNoches * calcularPrecioPorNoche(destino);
+	return precioDeEstadia;
 }
 
 // devuelve el precio por noche de acuerdo al destino pasado por parametro
 function calcularPrecioPorNoche(destino) {
-    let precioXNoche = 0;
-    switch (destino) {
-        case "mendoza":
-            precioXNoche = 3500;
-            break;
-        default:
-            precioXNoche = 4000;
-            break;
-    }
-    return precioXNoche;
+	let precioXNoche = 0;
+	switch (destino) {
+		case "mendoza":
+			precioXNoche = 3500;
+			break;
+		case "bariloche":
+			precioXNoche = 4500;
+			break;
+		case "cataratas":
+			precioXNoche = 2300;
+			break;
+		case "salta":
+			precioXNoche = 2900;
+		default:
+			precioXNoche = 0;
+			break;
+	}
+	return precioXNoche;
 }
 
-alert("El precio de la estadia es: " + calcularPrecioEstadia(destinoIngresado, cantidadNochesIngresadas));
+alert(
+	"El precio de la estadia es: " +
+		calcularPrecioEstadia(destinoIngresado, cantidadNochesIngresadas)
+);
