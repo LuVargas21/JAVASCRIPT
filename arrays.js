93e234e58c7fc042bb8a6b24e5d240c0f53ed0a5
LuVargas21 <96500749+LuVargas21@users.noreply.github.com>
2022-07-10 13:18:13 -0300

    objects

---
+++ arrays.js
@@ -0,0 +1,32 @@
+// CREACION DE OBJETOS
+
+let bariloche = {
+	destino: "nacional",
+	temporada: "invierno",
+    valorPorNoche: 3500, 
+};
+
+
+let buenosAires = {
+	destino: "nacional",
+	temporada: "todo el año",
+    valorPorNoche: 3500, 
+};
+
+let cataratas = {
+	destino: "nacional",
+	temporada: "primavera",
+    valorPorNoche: 3500, 
+};
+
+let mendoza = {
+	destino: "nacional",
+	temporada: "invierno",
+    valorPorNoche: 3500, 
+};
+
+let cordoba = { 
+	destino: "nacional",
+	temporada: "verano",
+    valorPorNoche: 3500, 
+};
